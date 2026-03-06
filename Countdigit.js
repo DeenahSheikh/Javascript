c8017a5e48811ee4ae1401a016800e2381f79637
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-03-06 22:36:54 +0530

    question

---
+++ Countdigit.js
@@ -0,0 +1,12 @@
+function countDigits(num) {
+    let count = 0;
+
+    while (num !== 0) {
+        num = Math.floor(num / 10);
+        count++;
+    }
+
+    return count;
+}
+
+console.log(countDigits(12345));
