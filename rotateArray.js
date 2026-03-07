7065c4b08e95065ecd47774dd096bf2531f59383
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-03-07 23:05:55 +0530

    Array

---
+++ rotateArray.js
@@ -0,0 +1,13 @@
+function rotateLeft(arr) {
+    let first = arr[0];
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    arr[arr.length - 1] = first;
+
+    return arr;
+}
+
+console.log(rotateLeft([1,2,3,4,5]));
